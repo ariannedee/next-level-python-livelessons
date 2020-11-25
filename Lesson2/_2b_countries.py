a3712f05ce2ba8ddbbd8cd1464cb9ca3f60e621f
ariannedee <ariannedee@gmail.com>
2020-11-25 15:58:21 -0800

    Update starting code

--- Lesson2/_2b_countries.py
+++ Lesson2/_2b_countries.py
@@ -3,22 +3,24 @@ A program that takes a letter and outputs a text file of
 all of the countries that start with that letter
 """
 
+countries = []
+
 # Read data/countries.txt and save all countries by starting letter
-countries = {}
-with open('data/countries.txt', 'r') as file:
+with open("data/countries.txt") as file:
     for line in file.readlines():
-        starting_letter = line[0]
-        country = line.strip()
-        if starting_letter not in countries:
-            countries[starting_letter] = []
-        countries[starting_letter].append(country)
+        countries.append(line.strip())
 
 # Get user to provide a letter
 letter = input('Number of countries that start with letter: ')
 letter = letter.capitalize()
 
 # Print the number of countries that start with the letter
-letter_countries = countries.get(letter, [])
+letter_countries = []
+for country in countries:
+    if country[0].upper() == letter:
+        letter_countries.append(country)
+
+# Print the number of countries that start with the letter
 print(f'{len(letter_countries)} countries start with an {letter}')
 
 # Todo: Create text file that lists the countries starting with the letter

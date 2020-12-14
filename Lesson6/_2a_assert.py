335f1d219b780bb1a4312ec71c0233e20c96e75b
ariannedee <ariannedee@gmail.com>
2020-12-14 10:59:02 -0800

    Don't loop over cases

--- Lesson6/_2a_assert.py
+++ Lesson6/_2a_assert.py
@@ -6,39 +6,27 @@ def assert_equals(actual, expected):
 
 
 def test_determine_winner():
-    test_cases = (
-        ('r', 'r', None),
-        ('r', 'p', COMP),
-        ('r', 's', YOU),
-        ('p', 'r', YOU),
-        ('p', 'p', None),
-        ('p', 's', COMP),
-        ('s', 'r', COMP),
-        ('s', 'p', YOU),
-        ('s', 's', None),
-    )
-
-    for case in test_cases:
-        you, comp, winner = case
-        assert_equals(determine_winner(you, comp), winner)
+    assert_equals(determine_winner('r', 'r'), None)
+    assert_equals(determine_winner('r', 'p'), COMP)
+    assert_equals(determine_winner('r', 's'), YOU)
+    assert_equals(determine_winner('p', 'r'), YOU)
+    assert_equals(determine_winner('p', 'p'), None)
+    assert_equals(determine_winner('p', 's'), COMP)
+    assert_equals(determine_winner('s', 'r'), COMP)
+    assert_equals(determine_winner('s', 'p'), YOU)
+    assert_equals(determine_winner('s', 's'), None)
 
     print('determine_winner tests passed')
 
 
 def test_game_over():
-    test_cases = (
-        (3, [0, 0], None),
-        (3, [1, 1], None),
-        (3, [2, 1], YOU),
-        (3, [1, 2], COMP),
-        (5, [2, 2], None),
-        (5, [3, 0], YOU),
-        (5, [1, 3], COMP),
-    )
-
-    for case in test_cases:
-        best_of, score, winner = case
-        assert_equals(game_over(best_of, score), winner)
+    assert_equals(game_over(3, [0, 0]), None)
+    assert_equals(game_over(3, [1, 1]), None)
+    assert_equals(game_over(3, [2, 1]), YOU)
+    assert_equals(game_over(3, [1, 2]), COMP)
+    assert_equals(game_over(5, [2, 2]), None)
+    assert_equals(game_over(5, [3, 0]), YOU)
+    assert_equals(game_over(5, [1, 3]), COMP)
 
     print('game_over tests passed')
 

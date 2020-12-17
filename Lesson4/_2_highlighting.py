5fc81e246ac1268cea5af1d391600a4bc4fc950d
ariannedee <ariannedee@gmail.com>
2020-12-17 10:56:54 -0800

    Update examples

---
+++ Lesson4/_2_highlighting.py
@@ -0,0 +1,10 @@
+import math
+from random import does_not_exist
+
+print(a_new_variable)
+
+def my_function(arg1, arg2):
+    print(arg1)
+    print(arg2)
+def another_function():
+    return
---
+++ Lesson4/_3_configuration.py
@@ -0,0 +1,5 @@
+import sys
+
+name = sys.argv[1]
+
+print(f'Hello {name}')

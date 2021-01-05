984b22a0340f4f71368a03ac376aad4b45fca208
ariannedee <ariannedee@gmail.com>
2021-01-05 15:37:49 -0800

    Update practice problem

--- Lesson6/_3_practice.py
+++ Lesson6/_3_practice.py
@@ -1,7 +1,20 @@
 """
-Write a function that takes a number and returns it as a string with commas as
-the thousands separators
+format_number is a function that takes a number and returns it as a string with
+commas as the thousands separators
 
-Basic practice: handle only positive whole numbers
-Extra practice: handle negative numbers and numbers with decimal places
+Basic practice: add tests and fix the function
+Extra practice: extend it to handle negative numbers and numbers with decimal places
 """
+
+num = input("Enter a number: ")
+
+
+as_string = ""
+i = 0
+for char in str(num):
+    if i % 3 == 1:
+        as_string += ','
+    as_string += char
+    i += 1
+
+print(as_string)
